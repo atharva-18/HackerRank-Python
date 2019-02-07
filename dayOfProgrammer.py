e22d9932829157893cf6a5d61a969e1bcc424c5e
AtharvaPusalkar <atharvapusalkar18@gmail.com>
2019-02-07 22:04:26 +0530

    Create 1 new file

---
+++ dayOfProgrammer.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the dayOfProgrammer function below.
+def dayOfProgrammer(year):
+    if year == 1918:
+        return "26.09.1918"
+    elif (year<1918 and year%4==0) or (year%4==0 and year%100!=0) or (year%100==0):
+        return "12.09."+str(year)
+    else:
+        return "13.09."+str(year)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    year = int(input().strip())
+
+    result = dayOfProgrammer(year)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
