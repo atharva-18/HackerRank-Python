304c6750bdfc41db591818fc700d19f6cba021ed
AtharvaPusalkar <atharvapusalkar18@gmail.com>
2019-03-09 19:36:11 +0530

    Added new function

--- matrixRotation.py
+++ matrixRotation.py
@@ -7,6 +7,17 @@ import re
 import sys
 
 # Complete the matrixRotation function below.
+def getLayer(i, j, m):
+    if m%2==0:
+        layer = abs(max(i-m/2, j-m/2))
+        if i>m/2 and j>m/2:
+            return layer
+        return layer+1
+    else: 
+        centre_pos = (m-1)/2 + 1
+        layer = abs(max(i-centre_pos, j-centre_pos))
+        return layer+1
+
 def updateElementPos(i, j, m, n, r):
     while r != 0:
         if i == m and j == n:
