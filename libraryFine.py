b9a8c7da5dcbe5c6a7fdcd74b2c6a11085999e63
AtharvaPusalkar <atharvapusalkar18@gmail.com>
2019-03-14 16:59:05 +0530

    Create 1 new file

---
+++ libraryFine.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+# Complete the libraryFine function below.
+def libraryFine(d1, m1, y1, d2, m2, y2):
+    if y1==y2:
+        if m1==m2:
+            if d1<=d2:
+                return 0
+            else:
+                return 15*(d1-d2)
+        elif m1<m2:
+            return 0
+        else:
+            return 500*(m1-m2)
+    elif y1<y2:
+        return 0
+    else:
+        return 10000
+
+d1M1Y1 = input().split()
+
+d1 = int(d1M1Y1[0])
+
+m1 = int(d1M1Y1[1])
+
+y1 = int(d1M1Y1[2])
+
+d2M2Y2 = input().split()
+
+d2 = int(d2M2Y2[0])
+
+m2 = int(d2M2Y2[1])
+
+y2 = int(d2M2Y2[2])
+
+print(libraryFine(d1, m1, y1, d2, m2, y2))
+
