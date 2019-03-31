8ba5526befb5924fd6b1579978822da3dd730a85
AtharvaPusalkar <atharvapusalkar18@gmail.com>
2019-03-31 08:25:36 +0530

    Create 1 new file

---
+++ findingThePercentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        line = input().split()
+        name, scores = line[0], line[1:]
+        scores = map(float, scores)
+        student_marks[name] = scores
+    query_name = input()
+
+score_card = student_marks[query_name]
+print(format(sum(score_card)/3, '.2f'))
